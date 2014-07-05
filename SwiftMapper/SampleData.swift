1bc65a2ec187368a3775345234a7d8eb6f08c5b0
Kamil Borzym <kamil.borzym@gmail.com>
2014-07-05 15:46:21 +0200

    Gitignore

---
+++ SwiftMapper/SampleData.swift
@@ -0,0 +1,32 @@
+//
+//  SampleData.swift
+//  SwiftMapper
+//
+//  Created by Kamil Borzym on 05.07.2014.
+//  Copyright (c) 2014 Swiftcrunch. All rights reserved.
+//
+
+import Foundation
+
+var json = [ "users" : [
+    [   "id" : 1,
+        "name" : "Kamil",
+        "email" : "kamil@gmail.com",
+        "photo" : "http://imgur.com/photo1.jpg",
+        "birthdate" : "1986-09-29"],
+    [   "id" : 2,
+        "name" : "Antonio",
+        "email" : "antonia@gmail.com",
+        "photo" : "http://imgur.com/photo2.jpg",
+        "birthdate" : "1980-09-29"],
+    [   "id" : 3,
+        "name" : "Maciej",
+        "email" : "maciej@gmail.com",
+        "photo" : "http://imgur.com/photo3.jpg",
+        "birthdate" : "1978-03-09"],
+    [   "id" : 4,
+        "name" : "Lukasz",
+        "email" : "lukasz@gmail.com",
+        "photo" : "http://imgur.com/photo4.jpg",
+        "birthdate" : "1972-01-17"]
+    ]]
